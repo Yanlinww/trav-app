5a0e54b5d8f4b836d6aecb157dd124694c07e40f
Yanlinww <eric831169@gmail.com>
2026-05-12 20:43:45 +0800

    AAB

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-        Welcome 羅傑!
+        Welcome 羅傑ROGER!
       </h1>
     </div>
   );
