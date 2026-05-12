18a687057efacfb07fb1f7b414b9fcbb099b8107
Yanlinww <eric831169@gmail.com>
2026-05-12 21:57:43 +0800

    ROG靠北

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
         Welcome 羅傑ROGER!!!!!!!!!
-        大雞巴
+        大家好！歡迎來到我的個人網站！這裡是我分享我的興趣、經歷和想法的地方。希望你能在這裡找到有趣的內容，並且享受瀏覽的過程！
       </h1>
     </div>
   );
