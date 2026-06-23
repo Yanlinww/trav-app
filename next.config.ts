49bfcd1df9cd2b32370968275dd7be7ef0d85fe8
Yanlinww <eric831169@gmail.com>
2026-06-23 23:31:09 +0800

    555

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+  // 🎫 只保留：放行組員的特定 IP，讓他可以跨網域連線
   allowedDevOrigins: ['26.133.163.81'],
-}
+
+};
 
 export default nextConfig;
